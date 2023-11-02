94cf90f83db49e121dfae546ab60f42fb9981eff
Etwashoeren <sungmin0609@nate.com>
2023-11-02 14:42:46 +0900

    feat.PersonalComputer, Moniter, Computer 클래스 구현

---
+++ Homework5.java
@@ -0,0 +1,42 @@
+public class Homework5 {
+    class PersonalComputer{
+
+    }
+
+    class Moniter{
+        private String moniterSize;
+        private String color;
+
+        private String power;
+
+        public Moniter(String moniterSize, String color, String power){
+            this.moniterSize = moniterSize;
+            this.color = color;
+            this.power = power;
+        }
+
+        public void turnOn(){
+
+        }
+    }
+
+    class Computer{
+        private String cpu;
+        private String memory;
+        private String hd;
+        private String color;
+        private String power;
+
+        public Computer(String cpu, String memory, String hd, String color, String power){
+            this.cpu = cpu;
+            this.memory = memory;
+            this.hd = hd;
+            this.color = color;
+            this.power = power;
+        }
+
+        public void turnOn(){
+
+        }
+    }
+}
